aef7770d264860e3fa60445d8be42e63e73de85c
Jason <jmcadam03@qub.ac.uk>
2019-03-13 11:58:39 +0000

    WSGI: Roll back to wsgi and use http requests

--- qub_model_back/asgi.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-import channels.asgi
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qub_model_back.settings")
-django.setup()
-
-channel_layer = channels.asgi.get_channel_layer()
