72edf4ebba934396ebcee40f77c59132616e89f4
Jason <jmcadam03@qub.ac.uk>
2019-03-16 15:56:22 +0000

    Deployment: Update requirements : : add urllib3

--- routing.py
+++
@@ -1,7 +0,0 @@
-from .qub_model_back import consumers
-
-channel_routing = {
-    'websocket.connect': consumers.ws_connect,
-    'websocket.receive': consumers.ws_receive,
-    'websocket.disconnect': consumers.ws_disconnect,
-}
